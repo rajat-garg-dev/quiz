4e783110a1dbfdd06686e38fbe251fa9d2e70423
rajat-garg-dev <gargrajat781@gmail.com>
2024-02-22 19:28:14 +0530

    created the slice and the global state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useGetQuestionQuery } from "./slices/questionlist";
+import Quiz from "./components/quiz";
 function App() {
-  const userID = 1;
-  const quizId = 1;
-  const { data: questionlist } = useGetQuestionQuery({ userID, quizId });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <Quiz />
     </div>
   );
 }
---
+++ frontend/src/slices/quizreducerslice.js
@@ -0,0 +1,97 @@
+import { createSlice, current } from "@reduxjs/toolkit";
+
+const initialState = {
+  userId: 0,
+  quizId: 0,
+  questions: [],
+  createdAt: "",
+  id: 0,
+  updatedAt: "",
+};
+
+const quizreducerslice = createSlice({
+  name: "quiz",
+  initialState,
+  reducers: {
+    setter(state, { payload }) {
+      const { userId, quizId, questions } = payload;
+      state.quiz = {
+        userId: userId,
+        quizId: quizId,
+        questions: questions?.map((q) => {
+          const { correctAnswer, incorrectAnswers } = q;
+          const options = [...incorrectAnswers];
+          const data = options.map((data) => ({
+            value: data,
+            correct: false,
+          }));
+          data.push({
+            value: correctAnswer,
+            correct: true,
+          });
+          return {
+            ...q,
+            options: data,
+            userFlag: false,
+            userAnswer: [],
+            attempt: 3,
+            score: 0,
+          };
+        }),
+      };
+      console.log("payload", payload);
+      return { ...state.quiz, payload };
+      // return { ...state.quiz, ...payload };
+    },
+    answerFun(state, { payload }) {
+      let { answer, id } = payload;
+      // answer = JSON.parse(answer);
+
+      // const { id, answer } = payload;
+      const existingQues = state.questions.find(
+        (question) => question.id === id
+      );
+      if (existingQues) {
+        existingQues.userAnswer.push(answer);
+      }
+      // return { ...state.quiz.questions, existingQues };
+    },
+    flagFun(state, { payload }) {
+      console.log("datasss");
+      const { id } = payload;
+      const existingQues = state.questions.find(
+        (question) => question.id === id
+      );
+      if (existingQues) {
+        existingQues.userFlag = !existingQues.userFlag;
+      }
+    },
+
+    attemptFun(state, { payload }) {
+      let { id } = payload;
+      // answer = JSON.parse(answer);
+
+      // const { id, answer } = payload;
+      const existingQues = state.questions.find(
+        (question) => question.id === id
+      );
+      if (existingQues) {
+        existingQues.attempts = existingQues.attempts - 1;
+      }
+    },
+    scoreFun(state, { payload }) {
+      const { id } = payload;
+      const existingQues = state.questions.find(
+        (question) => question.id === id
+      );
+      if (existingQues) {
+        existingQues.score = 1;
+      }
+    },
+  },
+});
+
+export const { setter, answerFun, flagFun, attemptFun, scoreFun, optionsFun } =
+  quizreducerslice.actions;
+export const quizSelector = (state) => state.quiz;
+export default quizreducerslice.reducer;
